12f2c7905273ab5b10ca671bfbdf7b4c7dd326a7
Stephen Reynolds <mail@stephenreynolds.dev>
2026-02-02 01:30:05 -0500

    Add solution to daily 2026-02-02

---
+++ daily/2026-02-02/HelloCSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("What is your name? ");
+string? name = Console.ReadLine();
+
+if (name is null)
+{
+    Console.WriteLine("Nothing was read. Exiting.");
+    return;
+}
+
+Console.WriteLine($"Hello, {name}!");
+
+var datetime = DateTime.Now;
+Console.WriteLine(
+    $"Today is {datetime.ToString("dddd, MMMM d, yyyy")} at {datetime.ToString("h:mm tt")}."
+);
